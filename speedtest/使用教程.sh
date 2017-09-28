017ebed1a0d79bddf10b6cabd1560e6fdb3010df
21ki <21kixc@gmail.com>
2017-09-28 11:11:28 +0800

    speedtest

---
+++ speedtest/使用教程.sh
@@ -0,0 +1,7 @@
+wget https://raw.github.com/sivel/speedtest-cli/master/speedtest.py
+chmod a+rx speedtest.py
+mv speedtest.py /usr/local/bin/speedtest
+chown root:root /usr/local/bin/speedtest
+speedtest-cli --list|more
+speedtest-cli
+speedtest
