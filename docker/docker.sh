c6549a6ce1476597220d208c12a5a579259bf2ae
vosclicent <21kixc@gmail.com>
2018-05-15 10:10:36 +0800

    Update docker.sh

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,3 +2,5 @@
 docker inspect --format='{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
 #提交新的镜像
 docker commit c3f279d17e0a  tomcat-8
+#启动系统所有镜像
+docker start $(docker ps -a -q)
