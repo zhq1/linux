cb77da81eee8ed15dab5a5017ee95c09495a089e
vosclicent <21kixc@gmail.com>
2018-05-25 13:37:16 +0800

    Update docker.sh

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,3 +4,7 @@ docker inspect --format='{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAdd
 docker commit c3f279d17e0a  tomcat-8
 #启动系统所有镜像
 docker start $(docker ps -a -q)
+#docker安装ELK
+git clone https://github.com/deviantony/docker-elk.git
+cd docker-elk/
+docker-compose up -d
