19ae31a4dfe53e83981708979f3dd52fc3933106
vosclicent <21kixc@gmail.com>
2018-06-15 16:16:28 +0800

    Create ipmitool.sh

---
+++ ipmitool/ipmitool.sh
@@ -0,0 +1,11 @@
+#DELL R730 服务器IPMI接口
+yum install -y ipmitool
+ipmitool lan print
+ipmitool lan set 1 ipaddr 192.168.2.121
+ipmitool lan set 1 mask 22
+ipmitool lan set 1 mask 255.255.252.0
+ipmitool lan set 1 netmask 255.255.252.0
+ipmitool lan set 1 defgw ipaddr 192.168.1.1
+ipmitool mc reset cold
+#默认密码
+#root/calvin
