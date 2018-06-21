d4d7a5e7b9ce9f88d1e123b8e0bd12f69c358a19
vosclicent <21kixc@gmail.com>
2018-06-21 11:38:34 +0800

    Update rsync.sh

--- rsync/rsync.sh
+++ rsync/rsync.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 #获取192.168.2.4 /datawww/logs下面的文件到home下
 rsync -av root@192.168.2.4:/data/wwwlogs /home
+#上传/root/myki本地文件夹到192.168.2.4的/data下
+rsync -av /root/myki root@192.168.2.4:/data/
