d2168639d22283fbd901a6de7270f1311d3a8293
vosclicent <21kixc@gmail.com>
2018-06-24 19:16:04 +0800

    Update pro.sh

--- svn/pro.sh
+++ svn/pro.sh
@@ -4,10 +4,11 @@ while true; do
 	svns=`netstat -anlp|grep 3690|grep -v grep`
 	if [ ! "$svns" ]; then
 		#如果不存在就重新启动
-		nohup svnserve -d -r /var/svn/svnrepos &
+		nohup svnserve -d -r /var/svn/ &
 		sleep 10
 		#启动后沉睡10秒
 	fi
 	#每次循环沉睡5秒
 	sleep 5
 done
+#nohup /opt/pro.sh >/dev/null 2>&1 &
