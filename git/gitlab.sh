5d5980d1c542c94249724ae740e0b0236c689439
vosclicent <21kixc@gmail.com>
2018-07-02 23:00:51 +0800

    Create gitlab.sh

---
+++ git/gitlab.sh
@@ -0,0 +1,31 @@
+vim /etc/yum.repos.d/gitlab-ce.repo
+[gitlab-ce]
+name=Gitlab CE Repository
+baseurl=https://mirrors.tuna.tsinghua.edu.cn/gitlab-ce/yum/el$releasever/
+gpgcheck=0
+enabled=1
+
+yum makecache
+#自动安装最新版
+yum install gitlab-ce        
+
+vim /etc/gitlab/gitlab.rb
+external_url 'http://118.190.164.99'
+
+gitlab-ctl reconfigure
+gitlab-ctl start
+
+#默认web登陆密码
+root 和 5iveL!fe
+
+
+
+GitLab常用命令
+sudo gitlab-ctl start    # 启动所有 gitlab 组件；
+sudo gitlab-ctl stop        # 停止所有 gitlab 组件；
+sudo gitlab-ctl restart        # 重启所有 gitlab 组件；
+sudo gitlab-ctl status        # 查看服务状态；
+sudo gitlab-ctl reconfigure        # 启动服务；
+sudo vim /etc/gitlab/gitlab.rb        # 修改默认的配置文件；
+gitlab-rake gitlab:check SANITIZE=true --trace    # 检查gitlab；
+sudo gitlab-ctl tail        # 查看日志；
