a0877bdd4a6788404cff63cb804408fbca308e63
Myki <hujialou@1nth.com>
2018-07-12 17:34:21 +0800

    docker20180712

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,5 +1,7 @@
 #查找docker ip
 docker inspect --format='{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
+docker inspect -f '{{.Name}} - {{.NetworkSettings.IPAddress }}' $(docker ps -aq)
+
 #提交新的镜像
 docker commit c3f279d17e0a  tomcat-8
 #启动系统所有镜像
