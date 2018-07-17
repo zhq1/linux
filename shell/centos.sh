ba28f744a0dda5d60a9007644dad67eed1531c4e
Myki <hujialou@1nth.com>
2018-07-17 14:04:10 +0800

    shell_20180717增加cp说明

--- shell/centos.sh
+++ shell/centos.sh
@@ -1,2 +1,9 @@
 hostnamectl set-hostname  test  #修改主机名
 nohup python -m SimpleHTTPServer 8001 &   #启动一个http
+#把文件夹1并且附带文件夹1拷贝到2（1和2都是文件夹）
+cp -dprf 1 2
+#把文件3拷贝到2下面
+cp -dprf 3 2
+#把1文件夹下面的文件不附带文件夹1拷贝到2
+cp -dprf 1/* 2/
+cp -dprf 1/ 2/
