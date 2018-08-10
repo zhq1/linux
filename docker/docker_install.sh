7e88ec12ea8dd045371e4585fd20e2739ccca098
cheche <cheche@e470.com>
2018-08-10 10:47:07 +0800

    docker_install

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -9,4 +9,14 @@ yum makecache
 #下载特定版本docker
 #下载特定版本docker
 wget https://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/edge/Packages/docker-ce-17.06.2.ce-1.el7.centos.x86_64.rpm
-#test
+#安装docker
+yum localinstall docker-ce-17.06.2.ce-1.el7.centos.x86_64.rpm  -y
+#因为docker镜像被墙所以配置加速器
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://9mmj7d8t.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
