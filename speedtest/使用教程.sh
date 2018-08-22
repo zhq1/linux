59f76b835b80fe4deb3bc7074ee5db8066e179d0
Myki <sjlh>
2018-08-22 21:01:35 +0800

    speedtest

--- speedtest/使用教程.sh
+++ speedtest/使用教程.sh
@@ -1,5 +1,6 @@
-wget https://raw.github.com/sivel/speedtest-cli/master/speedtest.py
-chmod a+rx speedtest.py
+wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py --no-check-certificate
+chmod +x speedtest-cli
+./speedtest-cli
 mv speedtest.py /usr/local/bin/speedtest
 chown root:root /usr/local/bin/speedtest
 speedtest-cli --list|more
