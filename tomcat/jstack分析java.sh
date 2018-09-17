5730de1093cdc93fe416d29b42c88f5bee9240df
Myki <sjlh>
2018-09-17 16:53:11 +0800

    java_cpu

--- tomcat/jstack分析java.sh
+++ tomcat/jstack分析java.sh
@@ -1,3 +1,16 @@
+#闲来无事查看java占用cpu问题查到好用的工具mark一下
+#参考连接https://www.cnblogs.com/lishijia/p/5549980.html
+top
+#pid 8526
+ps -mp 8526 -o THREAD,tid,time
+#pid转换成16进制
+printf "%x\n" 8534
+#分析代码
+jstack 8526 |grep 2156 -A 30
+
+jstack 8526 |grep 'printf "%x\n" 8534' -A 30
+
+
 #参看连接https://blog.csdn.net/gkqqq/article/details/76572523
 #top获取占用最大cpu的进程
 top
