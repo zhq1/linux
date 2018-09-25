fe411fc7778908c51a79ac2d6370e86b68d5a438
Myki <sjlh>
2018-09-25 14:18:23 +0800

    sjlh

--- sjlh/ssh-key.sh
+++ sjlh/ssh-key.sh
@@ -1,2 +1,12 @@
 #ssh免密登陆
 ssh-copy-id -i ~/.ssh/id_rsa.pub webadm@192.168.1.69
+#linux远程执行命令
+ssh webadm@192.168.1.45 "cd /home/ ; ls"
+#有或者
+#!/bin/bash
+ssh user@remoteNode > /dev/null 2>&1 << eeooff
+cd /home
+touch abcdefg.txt
+exit
+eeooff
+echo done!
