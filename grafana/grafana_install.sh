212aea89a4934d808ab797a32c01f3ad2aeb6526
Myki <sjlh>
2018-09-27 15:22:32 +0800

    grafana

--- grafana/grafana_install.sh
+++ grafana/grafana_install.sh
@@ -3,3 +3,7 @@
 yum install initscripts fontconfig
 wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.1.4-1.x86_64.rpm
 yum install grafana-5.1.4-1.x86_64.rpm
+#更新配置
+systemctl daemon-reload
+systemctl start grafana-server
+systemctl status grafana-server
