c39353d2e968ceda2a1313960688b83094de1682
Myki <sjlh>
2018-09-27 15:12:03 +0800

    grafana

---
+++ grafana/grafana_install.sh
@@ -0,0 +1,5 @@
+#centos 7.5 
+#granfana 5.1.4安装
+yum install initscripts fontconfig
+wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.1.4-1.x86_64.rpm
+yum install grafana-5.1.4-1.x86_64.rpm
